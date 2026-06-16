200a4714447092191bbb57309695f97f5f26d97b
Giovanni Posabella <gposabella@gmail.com>
2026-06-16 11:49:15 +0200

    varie

--- src/main/java/com/cimmino/shop/database/DDTRepository.java
+++ src/main/java/com/cimmino/shop/database/DDTRepository.java
@@ -4,11 +4,20 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface DDTRepository extends JpaRepository<DDT, Long> {
 
 	@Query("select g from DDT g where g.numeroDDT = ?1")
 	Optional<DDT> findByNumDDT(String dttNum);
+
+	
+	@Query("""
+			select count(d) > 0
+			from DDT d
+			where d.numeroDDT = :numero
+			""")
+			boolean existsByNumeroDDT(@Param("numero") String numero);
 
 	
 }
